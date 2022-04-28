85e95ffd65d371139d737f4112dd867896cd0efa
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-28 19:36:38 +0530

    Added: Validating last name

--- main/java/com/bridgelabz/junittesting/UserValidator.java
+++ main/java/com/bridgelabz/junittesting/UserValidator.java
@@ -4,11 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 /*
  *Author:Prasad
- *
  */
 public class UserValidator {
     //regex patterns
     private static final String regexFirstName = "^[A-Z]{1}[a-z]{2,}";
+    private static final String regexLastName = "^[A-Z]{1}[a-z]{2,}$";
 
     /*
      * To validate first name
@@ -16,5 +16,13 @@ public class UserValidator {
     public boolean firstName(String firstName){
         Pattern pattern = Pattern.compile(regexFirstName);
         return  pattern.matcher(firstName).matches();
+    }
+
+    /*
+     * To validate last name
+     */
+    public boolean lastName(String lastName){
+        Pattern pattern = Pattern.compile(regexLastName);
+        return  pattern.matcher(lastName).matches();
     }
 }
