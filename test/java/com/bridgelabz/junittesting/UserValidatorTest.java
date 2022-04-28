2fb9a486b356603a632de492f99490ad07f0a10e
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-28 19:38:03 +0530

    Added: validating last name using j unit test

--- test/java/com/bridgelabz/junittesting/UserValidatorTest.java
+++ test/java/com/bridgelabz/junittesting/UserValidatorTest.java
@@ -1,18 +1,25 @@
 package com.bridgelabz.junittesting;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /*
  *Author: Prasad
  */
 public class UserValidatorTest {
+    UserValidator userValidator;
+    //before method
+    @Before
+    public void before(){
+        userValidator = new UserValidator();
+    }
+
     /*
      * If first name is proper then this method shoud pass the asserttrue condition
      */
     @Test
     public void givenFirstName_WhenProper_ReturnTrue(){
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.firstName("Prasad");
         Assert.assertTrue(result);
     }
@@ -22,8 +29,25 @@ public class UserValidatorTest {
      */
     @Test
     public void givenFirstName_WhenNotProper_ReturnFalse(){
-        UserValidator userValidator = new UserValidator();
         boolean result = userValidator.firstName("ma");
+        Assert.assertFalse(result);
+    }
+
+    /*
+     * If last name is proper then this method shoud pass the asserttrue condition
+     */
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue(){
+        boolean result = userValidator.lastName("Somvanshi");
+        Assert.assertTrue(result);
+    }
+
+    /*
+     * If last name is not proper then this method shoud pass the assertfalse condition
+     */
+    @Test
+    public void givenLastName_WhenNotProper_ReturnFalse(){
+        boolean result = userValidator.lastName("somvanshi");
         Assert.assertFalse(result);
     }
 }
